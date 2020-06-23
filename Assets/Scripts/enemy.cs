d02d89881cb6aac0eb19801f1700dda8f8cd04e2
WinchesterOP <neco.giedrojc@gmail.com>
2020-06-23 21:35:28 +0200

    First Movement of the Enemy
    First 2 Scripts has been added

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+	public float speed = 10f; //Geschwindigkeitsvariable
+	public float deltaDistance = 0.6f;
+
+	private Transform target; //Variable um das nächste Ziel anzuwählen
+	private int waypointIndex = 0; //Temp-Zähler
+
+	void Start ()
+	{
+		target = waypoints.points[waypointIndex];
+	}
+
+	void Update ()
+	{
+		Vector3 direction = target.position - transform.position; // Ziel Position - aktuelle Position um einen Vektor zu bekommen
+		/**
+		 * Translate wird gentzt um eine Objekt zu bewegen mit einem Vektor
+		 * normalized ist dafür da eine gleichmäßige Bewegung durchzuführen
+		 * Space.World = die Welt in der sich das Objekt bewegt. Unsere Spielewelt
+		 */
+		transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance(target.position,transform.position) <= deltaDistance)
+        {
+			GetNextWaypoint();
+        }
+	}
+
+	void GetNextWaypoint()
+    {
+		if (waypointIndex > waypoints.points.Length - 1)
+        {
+			Destroy(gameObject);
+			return;
+        }
+
+		waypointIndex++;
+		target = waypoints.points[waypointIndex];
+    }
+}
